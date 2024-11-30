245718826e470ea3c58d580d9266a4f0f2fe6263
Vignesh <vighneshgupta32@gmail.com>
2024-11-30 11:08:10 +0530

    feat(prisma): add shortLink field to Link model; update unique constraints in Workspace model; upgrade Prisma and related dependencies

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@clikz/ui"],
+  images:{
+    remotePatterns: [
+      {
+        hostname: "avatars.githubusercontent.com",
+        protocol: "https",
+      },{
+        hostname: "cdn.discordapp.com",
+        protocol: "https",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
