c1bec98329dfc7dea3ad6813f11f2bc592076a8e
Vignesh <vighneshgupta32@gmail.com>
2024-12-01 12:32:34 +0530

    feat(eslint): enhance ESLint configuration with Prettier integration and additional rules; add Prettier config file

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,9 +1,13 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@clikz/eslint-config/next.js"],
+  extends: ["@clikz/eslint-config/next.js", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
+  },
+  rules: {
+    "prefer-arrow-callback": ["error"],
+    "prefer-template": ["error"],
   },
 };
