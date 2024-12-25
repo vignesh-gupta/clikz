53736b42b5c8bddd97a63e70f664440fa34c9450
Vignesh <vighneshgupta32@gmail.com>
2024-12-25 11:27:27 +0530

    feat(local-proxy): add initial setup for local proxy server with Express and HTTP proxy

---
+++ apps/local-proxy/index.ts
@@ -0,0 +1,25 @@
+// Express Boiler plate code
+
+import express from "express";
+import httpProxy from "http-proxy";
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const proxy = httpProxy.createProxy();
+
+app.use((req, res) => {
+  console.log(req.url);
+
+  const resolvesTo = `http://localhost:3000`;
+
+  return proxy.web(req, res, { target: resolvesTo });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
