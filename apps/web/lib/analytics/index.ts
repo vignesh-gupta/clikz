b2b6c8ea6fd0b2d732f723a5d6487574cf864840
Vighnesh Gupta <vighneshgupta32@gmail.com>
2025-02-19 20:57:17 +0530

    chore(dx): setup debug config

--- apps/web/lib/analytics/index.ts
+++ apps/web/lib/analytics/index.ts
@@ -1,5 +1,4 @@
 export * from "./click-events";
 
-// eslint-disable-next-line turbo/no-undeclared-env-vars
 export const TINYBIRD_API_KEY = process.env.TINYBIRD_API_KEY;
 export const TINYBIRD_PIPES_ENDPOINT = "https://api.tinybird.co/v0/pipes";
